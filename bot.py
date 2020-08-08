076ed029910a9d0fb2758654c693121e265337ec
grarich123 <grarich123+github@gmail.com>
2020-08-08 23:16:25 +0900

    flake8の警告の部分を修正

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 from os import environ
 from lib.database import Database
 
+
 class Aria(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix=commands.when_mentioned_or(environ.get('PREFIX', 'aria ')))
--- lib/database.py
+++ lib/database.py
@@ -1,4 +1,3 @@
-import asyncio
 import os
 from collections import namedtuple
 
