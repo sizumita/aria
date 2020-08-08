1b4548f3e5d0e673c728473019d43d4d3493c99d
sizumita <sumito@izumita.com>
2020-08-08 23:03:15 +0900

    spell.pyの変更
    コマンドの最低実行間隔を満たしているかの確認用の関数

--- lib/spell.py
+++ lib/spell.py
@@ -11,6 +11,13 @@ class Spell:
         self.last_aria_command_time = None
 
     def receive_command(self, command: str, aria_command_time: datetime.datetime):
+        """
+        コマンドを受け取り、自分のインスタンス変数を変化させ、Trueを返す
+        もしコマンドがおかしかった場合、Falseを返す。
+        :param command: コマンドの文
+        :param aria_command_time: コマンドを実行した時間
+        :return: bool
+        """
         if match := form_compile.match(command):
             self.form = match.group(0)
 
@@ -23,3 +30,18 @@ class Spell:
         self.last_aria_command_time = aria_command_time
 
         return True
+
+    def can_aria(self, will_aria_time: datetime.datetime):
+        """
+        制限時間15.0秒を過ぎていないかチェックする関数
+        :param will_aria_time: 次にコマンドを発動する時間
+        :return: bool
+        """
+
+        # 一度も実行されていなかった場合
+        if self.last_aria_command_time is None:
+            return True
+
+        diff = will_aria_time - self.last_aria_command_time
+
+        return True if diff.total_seconds() <= 15.0 else False
