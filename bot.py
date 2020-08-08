6e7d43aac0b85698b77bf2b0fa010da7dead974e
sizumita <sumito@izumita.com>
2020-08-08 19:59:25 +0900

    bot.pyの変更
    デフォルトのプレフィックスの設定

--- bot.py
+++ bot.py
@@ -4,4 +4,4 @@ from os import environ
 
 class Aria(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix=commands.when_mentioned_or(environ.get('PREFIX')))
+        super().__init__(command_prefix=commands.when_mentioned_or(environ.get('PREFIX', 'aria ')))
