182fe73fa4a9c2d4d86f856ab791531c76673479
sizumita <sumito@izumita.com>
2020-08-09 16:46:36 +0900

    mypyの修正

--- bot.py
+++ bot.py
@@ -5,11 +5,11 @@ import discord
 
 
 class Aria(commands.Bot):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__(command_prefix=commands.when_mentioned_or(
             environ.get('PREFIX', 'aria ')))
         self.db = Database()
 
-    async def on_ready(self):
+    async def on_ready(self) -> None:
         status = discord.Game("Aria - War of incantation")
         await self.change_presence(activity=status)
--- lib/database.py
+++ lib/database.py
@@ -1,6 +1,6 @@
 import os
 from typing import NamedTuple
-
+from typing import Union
 import asyncpg
 
 User = NamedTuple('User', [('id', int), ('hp', int), ('mp', int)])
@@ -8,25 +8,26 @@ User = NamedTuple('User', [('id', int), ('hp', int), ('mp', int)])
 
 class Database:
     """CREATE TABLE users (user_id bigint, hp integer, mp integer, PRIMARY KEY(user_id))"""
-    def __init__(self):
+    def __init__(self) -> None:
         self.db_url = os.environ['DATABASE_URL']
-        self.conn = None
+        self.conn: Union[asyncpg.Connection, None] = None
 
-    async def check_database(self):
+    async def check_database(self) -> None:
         conn = self.conn or await self.setup()
         try:
             await conn.execute('select "users"::regclass')
         except asyncpg.exceptions.UndefinedColumnError:
             await conn.execute('CREATE TABLE users (user_id bigint, hp integer, mp integer, PRIMARY KEY(user_id))')
 
-    async def setup(self):
+    async def setup(self) -> asyncpg.Connection:
         self.conn = await asyncpg.connect(self.db_url)
         return self.conn
 
-    async def close(self):
-        await self.conn.close()
+    async def close(self) -> None:
+        if self.conn is not None:
+            await self.conn.close()
 
-    async def get_user(self, user_id):
+    async def get_user(self, user_id: int) -> Union[None, User]:
         conn = self.conn or await self.setup()
         data = await conn.fetch('SELECT * FROM users WHERE user_id=$1', user_id)
         if not data:
@@ -35,12 +36,12 @@ class Database:
         target = list(data[0])
         return User(target[0], target[1], target[2])
 
-    async def create_user(self, user_id):
+    async def create_user(self, user_id: int) -> Union[None, User]:
         conn = self.conn or await self.setup()
         await conn.execute('INSERT INTO users (user_id, hp, mp) VALUES ($1, $2, $3)', user_id, 100, 100)
         return await self.get_user(user_id)
 
-    async def update_user(self, user_id, hp, mp):
+    async def update_user(self, user_id: int, hp: int, mp: int) -> Union[None, User]:
         conn = self.conn or await self.setup()
         await conn.execute('UPDATE users SET hp = $1, mp = $2 WHERE user_id = $3', hp, mp, user_id)
         return await self.get_user(user_id)
