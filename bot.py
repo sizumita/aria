ae205ddf7cf42fd3e634b67c1bd0884042bf4239
AttoCat <wazasiyo2525a@gmail.com>
2020-08-09 15:47:38 +0900

    import discord、ステータス変更記述

--- bot.py
+++ bot.py
@@ -1,9 +1,15 @@
 from discord.ext import commands
 from os import environ
 from lib.database import Database
+import discord
 
 
 class Aria(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix=commands.when_mentioned_or(environ.get('PREFIX', 'aria ')))
+        super().__init__(command_prefix=commands.when_mentioned_or(
+            environ.get('PREFIX', 'aria ')))
         self.db = Database()
+
+    async def on_ready(self):
+        status = discord.Game("Aria - War of incantation")
+        await self.change_presence(activity=status)
