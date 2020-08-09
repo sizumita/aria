9548cde0a34aa28772319e27f0084f056fe2f49c
sizumita <sumito@izumita.com>
2020-08-09 21:04:36 +0900

    mypyの修正

--- lib/spell.py
+++ lib/spell.py
@@ -1,16 +1,22 @@
+from __future__ import annotations
 import re
 import datetime
-from typing import NamedTuple
+from typing import NamedTuple, Union
 form_compiled = re.compile(r'^change element (sword|spear|bow|wall|rod)$')
 feature_compiled = re.compile(r'^change feature (flame|water|earth|light|umbra)$')
 
-Form = NamedTuple('Form', (('damage', int), ('defence', int)))
+
+class Form(NamedTuple):
+    damage: int
+    defence: int
+
+
 forms = {
     'sword': Form(10, 20),
     'spear': Form(20, 10),
     'bow': Form(5, 5),
     'wall': Form(2, 30),
-    'rod': (15, 15),
+    'rod': Form(15, 15),
 }
 
 # 各属性の強い属性
@@ -23,31 +29,43 @@ strong_features = {
 }
 
 
+def _calc_feature(num: Union[int, float], self_spell: Spell, enemy_spell: Spell) -> Union[int, float]:
+    if self_spell.feature is not None and enemy_spell.feature is not None:
+        if strong_features[self_spell.feature] == enemy_spell.feature:
+            num *= 1.2
+        elif strong_features[enemy_spell.feature] == self_spell.feature:
+            num *= 0.8
+
+    return num
+
+
 class Spell:
     def __init__(self) -> None:
-        self.form = None
-        self.feature = None
-        self.last_aria_command_time = None
+        self.form: Union[None, str] = None
+        self.feature: Union[None, str] = None
+        self.last_aria_command_time: Union[None, datetime.datetime] = None
 
-    def calculate_damage(self, enemy_spell) -> int:
-        total_damage = forms[self.form].damage
+    def calculate_damage(self, enemy_spell: Spell) -> int:
+        total_damage: Union[int, float]
+        if self.form is None:
+            total_damage = 2
+        else:
+            total_damage = forms[self.form].damage
 
         # 属性有利不利
-        if strong_features[self.feature] == enemy_spell.feature:
-            total_damage *= 1.2
-        elif strong_features[enemy_spell.feature] == self.feature:
-            total_damage *= 0.8
+        total_damage = _calc_feature(total_damage, self, enemy_spell)
 
         return int(total_damage)  # 少数になる可能性もあるため
 
-    def calculate_defence(self, enemy_spell) -> int:
-        total_defence = forms[self.form].defence
+    def calculate_defence(self, enemy_spell: Spell) -> int:
+        total_defence: Union[int, float]
+        if self.form is None:
+            total_defence = 2
+        else:
+            total_defence = forms[self.form].defence
 
         # 属性有利不利
-        if strong_features[self.feature] == enemy_spell.feature:
-            total_defence *= 1.2
-        elif strong_features[enemy_spell.feature] == self.feature:
-            total_defence *= 0.8
+        total_defence = _calc_feature(total_defence, self, enemy_spell)
 
         return int(total_defence)  # 少数になる可能性もあるため
 
