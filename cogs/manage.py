a8c5c45762256d22ac70faeb93e83da17f1cb76a
daima3629 <daima3629@usbc.be>
2020-08-09 10:57:09 +0900

    [Add] `status`コマンドを作成

--- cogs/manage.py
+++ cogs/manage.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import textwrap
 
 
 class ManageCog(commands.Cog):
@@ -14,6 +15,19 @@ class ManageCog(commands.Cog):
         self.db.create_user(ctx.author.id)
         await ctx.send("ユーザー登録を行いました")
         # メッセージは要検討
+
+    @commands.command()
+    async def status(self, ctx):
+        user = self.db.get_user(ctx.author.id)
+        if user is None:
+            return ctx.send("あなたはまだ登録されていません")
+        
+        msg_text = f"""\
+        {ctx.author.mention} さんのステータス
+        HP: {user.hp}HP
+        MP: {user.mp}MP
+        """
+        return await ctx.send(textwrap.dedent(msg_text))
 
 
 def setup(bot):
