7b1512d827891077dca22ac660d9bd5da2f00c10
sevenc-nanashi <sevenc_nanashi@yahoo.co.jp>
2020-08-09 15:10:23 +0900

    on_readyとcchange_presenceを追加

--- bot.py
+++ bot.py
@@ -7,3 +7,5 @@ class Aria(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix=commands.when_mentioned_or(environ.get('PREFIX', 'aria ')))
         self.db = Database()
+    async def on_ready(self):
+        await client.change_presence(activity=discord.Game(name='Aria - War of incantation'))
