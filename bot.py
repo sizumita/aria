e4e2fa716e280c77d865f3f4ac20891c3d07397e
sevenc-nanashi <sevenc_nanashi@yahoo.co.jp>
2020-08-09 15:32:13 +0900

    on_readyとchange_presenceを追加（色々なミスを修正）

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 from os import environ
 from lib.database import Database
@@ -7,3 +8,6 @@ class Aria(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix=commands.when_mentioned_or(environ.get('PREFIX', 'aria ')))
         self.db = Database()
+    
+    async def on_ready(self):
+        await client.change_presence(activity=discord.Game(name='Aria - War of incantation'))
