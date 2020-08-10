e53c20ac1a1029725512cf91c5b3e988caf033e1
sizumita <sumito@izumita.com>
2020-08-10 12:24:49 +0900

    ゲームの文字の送信先を依存しないように

--- lib/game.py
+++ lib/game.py
@@ -1,8 +1,7 @@
-from discord.ext import commands
 import discord
 from typing import Any, Callable, Optional
 from lib.spell import Spell
-from asyncio import Task, Event, sleep
+from asyncio import Task, Event, sleep, iscoroutinefunction
 
 
 def _calc_damage(my_spell: Optional[Spell], enemy_spell: Optional[Spell]) -> int:
@@ -19,13 +18,17 @@ def _calc_damage(my_spell: Optional[Spell], enemy_spell: Optional[Spell]) -> int
 
 
 class Game:
-    def __init__(self, bot: Any, alpha: discord.Member, beta: discord.Member, ctx: commands.Context) -> None:
+    def __init__(self,
+                 bot: Any,
+                 alpha: discord.Member,
+                 beta: discord.Member,
+                 channel: discord.TextChannel,
+                 send_callable: Callable = print,
+                 ) -> None:
         self.bot = bot
         self.alpha = alpha
         self.beta = beta
-        self.ctx = ctx
-        self.channel = ctx.channel
-        self.guild = ctx.guild
+        self.channel = channel
         self.finish = False
         self.alpha_spell: Optional[Spell] = None
         self.beta_spell: Optional[Spell] = None
@@ -36,7 +39,14 @@ class Game:
         self.alpha_mp = 100
         self.beta_mp = 100
         self.ready_to_raise = False
+        self.send_callable = send_callable
         self.battle_finish_flag = Event(loop=self.bot.loop)
+
+    async def send(self, *args, **kwargs) -> None:
+        if iscoroutinefunction(self.send_callable):
+            await self.send_callable(*args, **kwargs)
+        else:
+            self.send_callable(*args, **kwargs)
 
     def alpha_check(self, message: discord.Message) -> bool:
         return message.channel.id == self.channel.id and message.author.id == self.alpha.id
@@ -50,7 +60,7 @@ class Game:
             message: discord.Message = await self.bot.wait_for('message', check=check, timeout=60)
             if message.content == 'execute':
                 if not self.use_mp(user, 5):
-                    await self.ctx.send('MPが枯渇しました。')
+                    await self.send('MPが枯渇しました。')
                     return None
                 break
 
@@ -58,9 +68,9 @@ class Game:
                 return None
 
             if mp := spell.receive_command(message.content, message.created_at):
-                await self.ctx.send('コマンドを受け取りました。')
+                await self.send('コマンドを受け取りました。')
                 if not self.use_mp(user, mp):
-                    await self.ctx.send('MPが枯渇しました。')
+                    await self.send('MPが枯渇しました。')
                     return None
                 continue
 
@@ -78,26 +88,28 @@ class Game:
         alpha_to_beta_damage = _calc_damage(self.alpha_spell, self.beta_spell)
         beta_to_alpha_damage = _calc_damage(self.beta_spell, self.alpha_spell)
 
-        await self.ctx.send(f'{self.alpha.mention} から {self.beta.mention} に {alpha_to_beta_damage} ダメージ！')
-        await self.ctx.send(f'{self.beta.mention} から {self.alpha.mention} に {beta_to_alpha_damage} ダメージ！')
+        await self.send(f'{self.alpha.mention} から {self.beta.mention} に {alpha_to_beta_damage} ダメージ！')
+        await self.send(f'{self.beta.mention} から {self.alpha.mention} に {beta_to_alpha_damage} ダメージ！')
 
         self.alpha_hp -= beta_to_alpha_damage
         self.beta_hp -= alpha_to_beta_damage
 
         if self.alpha_hp <= 0 and self.beta_hp <= 0:
-            await self.ctx.send('相打ち！両者HPが0になったため、相打ちとなります。')
+            await self.send('相打ち！両者HPが0になったため、相打ちとなります。')
             self.finish = True
         elif self.alpha_hp <= 0:
-            await self.ctx.send(f'{self.beta.mention} の勝ち！')
+            await self.send(f'{self.beta.mention} の勝ち！')
             self.finish = True
         elif self.beta_hp <= 0:
-            await self.ctx.send(f'{self.alpha.mention} の勝ち！')
+            await self.send(f'{self.alpha.mention} の勝ち！')
             self.finish = True
         else:
-            await self.ctx.send(f'{self.alpha.mention}\n HP: {self.alpha_hp}\n MP: {self.alpha_mp}',
-                                allowed_mentions=discord.AllowedMentions(users=False))
-            await self.ctx.send(f'{self.beta.mention}\n HP: {self.beta_hp}\n MP: {self.beta_mp}',
-                                allowed_mentions=discord.AllowedMentions(users=False))
+            await self.send(
+                f'{self.alpha.mention}\n HP: {self.alpha_hp}\n MP: {self.alpha_mp}',
+                allowed_mentions=discord.AllowedMentions(users=False)
+            )
+            await self.send(f'{self.beta.mention}\n HP: {self.beta_hp}\n MP: {self.beta_mp}',
+                            allowed_mentions=discord.AllowedMentions(users=False))
 
         self.battle_finish_flag.set()
         self.battle_finish_flag.clear()
@@ -125,23 +137,23 @@ class Game:
             message = await self.bot.wait_for('message', check=check, timeout=60)
             if message.content != 'aria command':
                 continue
-            await self.ctx.send('魔法の発動を開始します。')
+            await self.send('魔法の発動を開始します。')
             message = await self.bot.wait_for('message', check=check, timeout=60)
 
             if message.content != 'generate element':
-                await self.ctx.send('魔法の発動に失敗しました。')
+                await self.send('魔法の発動に失敗しました。')
                 continue
 
             if not self.use_mp(user):
-                await self.ctx.send('MPが枯渇しました。')
+                await self.send('MPが枯渇しました。')
                 return
 
-            await self.ctx.send('物質生成が完了しました。')
+            await self.send('物質生成が完了しました。')
             spell = await self.recv_command(check, user)
             if spell is None:
-                await self.ctx.send('魔法の発動に失敗しました。')
+                await self.send('魔法の発動に失敗しました。')
                 continue
-            await self.ctx.send('魔法の発動を開始します。')
+            await self.send('魔法の発動を開始します。')
             if user == 'alpha':
                 self.alpha_spell = spell
             else:
@@ -159,6 +171,6 @@ class Game:
         self.beta_hp = beta_db_user.hp
         self.beta_mp = beta_db_user.mp
 
-        await self.ctx.send('ゲームスタート！')
+        await self.send('ゲームスタート！')
         self.bot.loop.create_task(self.loop(self.alpha_check, 'alpha'))
         self.bot.loop.create_task(self.loop(self.beta_check, 'beta'))
