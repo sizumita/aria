32fdf6e3620d1a06de3050d72cabb427fb812afa
sizumita <sumito@izumita.com>
2020-08-12 16:29:52 +0900

    呪文発動時にエラーが出ないように

--- bot.py
+++ bot.py
@@ -13,3 +13,12 @@ class Aria(commands.Bot):
     async def on_ready(self) -> None:
         status = discord.Game("Aria - War of incantation")
         await self.change_presence(activity=status)
+
+    async def on_command_error(self, context, exception):
+        if isinstance(exception, commands.CommandNotFound):
+            return
+
+        if isinstance(exception, commands.CommandOnCooldown):
+            return
+
+        await super(Aria, self).on_command_error(context, exception)
