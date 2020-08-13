7b79e71f7bf27a4afc54bd92f344337878e97e33
sevenc-nanashi <sevenc_nanashi@yahoo.co.jp>
2020-08-14 05:35:25 +0900

    メンションに変更,それに伴い形式を一部変更

--- cogs/manage.py
+++ cogs/manage.py
@@ -40,20 +40,20 @@ class ManageCog(commands.Cog):
     @commands.command()
     async def ranking(self, ctx: commands.Context) -> None:
         users_ranking = await self.db.get_user_rankings()
-        ranking_message = "```\n"
+        ranking_message = ""
 
         for user_ranking in users_ranking:
             user = self.bot.get_user(user_ranking[0].id)
             user_data = user_ranking[0]
 
-            ranking_message += f"{user_ranking[1]}位: {user}, HP: {user_data.hp}, MP: {user_data.mp}\n"
+            ranking_message += f"`{user_ranking[1]}位: `{user.mention}`, HP: {user_data.hp}, MP: {user_data.mp}`\n"
 
         if not discord.utils.find(lambda u: u[0].id == ctx.author.id, users_ranking):
             if rank := await self.db.get_user_ranking(ctx.author.id):
                 user_data = await self.bot.db.get_user(ctx.author.id)
-                ranking_message += f"\n{rank}位: {ctx.author}, HP: {user_data.hp}, MP: {user_data.mp}"
+                ranking_message += f"`{rank}位: `{ctx.author.mention}`, HP: {user_data.hp}, MP: {user_data.mp}`"
 
-        await ctx.send(ranking_message + "```")
+        await ctx.send(ranking_message)
 
 
 def setup(bot: Any) -> None:
